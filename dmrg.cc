115176da04d5b0c6614a90f088ff366bf034cf39
VictorChangQQ <yenvicpzk@gmail.com>
2017-12-11 22:35:58 -0600

    upload qmc

---
+++ dmrg.cc
@@ -0,0 +1,9 @@
+dmrg
+density matrix renomalization group meathod.
+Good at one dimensional problem.
+Somewhat useful to solve two dimensional problem.
+It might not possible to solve three dimensional problem.
+Complementary to Mean-Field Theory favors strong correlated system.
+My first goal to be proficient.
+From Dr. Miles.
+
---
+++ qmc.cc
@@ -0,0 +1,6 @@
+Quantum Mento Carlo
+complementary to DMRG for high dimensional problems. 
+Basd on path integral meathod, favoring for even numbers of fermions to converge the integral.
+Used randomly selected representative states to simulate all states integration.
+It can be great example to learn path integral.
+
