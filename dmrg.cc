5ff10cfea9593a9117cef8629b32489ff1946d91
VictorChangQQ <30300914+VictorChangQQ@users.noreply.github.com>
2018-01-09 02:21:11 -0600

    To be more specific for my goal

--- dmrg.cc
+++ dmrg.cc
@@ -6,4 +6,5 @@ It might not possible to solve three dimensional problem.
 Complementary to Mean-Field Theory favors strong correlated system.
 My first goal to be proficient.
 From Dr. Miles.
-
+Hope to use this codes to learn not only state-of-the-art DMRG calculation but also DMRG-like machine learning as well as pararell computation.
+ Pay my gradtitue and respect to Master. 
